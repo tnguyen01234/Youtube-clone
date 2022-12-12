bbc7e2c9b3906c338f841204d07741aade69ac85
TommyZenbookPro <nguyentommy735@gmail.com>
2022-12-12 12:02:43 +1100

    UI update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo = 'https://logos-world.net/wp-content/uploads/2020/04/YouTube-Emblem.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
